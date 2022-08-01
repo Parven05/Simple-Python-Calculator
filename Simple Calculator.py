de184ce7c60dc05639b809f1bdfd2493d6d60f41
Parven <parvendranofficial05@gmail.com>
2022-08-01 10:14:13 +0800

    initial commit

---
+++ Simple Calculator.py
@@ -0,0 +1,47 @@
+#Simple Python Calculator
+def select_operation(a, b):
+  user = input(str("Enter an Operation : "))
+  if user == "+":
+    print(float(a) + float(b))
+  elif user == "-":
+    print(float(a) - float(b))
+  elif user == "*":
+    print(float(a) * float(b))
+  elif user == "/":
+    print(float(a) / float(b))
+  elif user == "%":
+    print(float (a) % float(b))
+  elif user == "**":
+    print(float (a) ** float(b))
+  else:
+    print("Invalid Number") 
+    return(float(a, b))
+
+def print_instructions():
+  print("Simple Python Calculator")
+  print("************************")
+  print("\nOperation Symbols")
+  
+  a = [
+    ["Sum = +", "Subtract = -", "Multiply = *"],
+    ["Divison = /","Remainder = %","Power = **"]
+  ]
+
+  for row in a:
+    for col in row:
+      print(col)
+
+#main
+print_instructions()
+
+try:
+  first_number = float(input("\nEnter First Number : "))
+  second_number = float(input("Enter Second Number : "))
+except:
+  print("Invalid number")
+
+select_operation(float(first_number), float(second_number))
+
+
+
+
